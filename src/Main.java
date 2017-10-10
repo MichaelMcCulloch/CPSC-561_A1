6affee80070d55d828b53b5d8e84561effd9b0fd
Michael McCulloch <michael.james.mcculloch@gmail.com>
2017-10-09 20:49:59 -0600

    Assignment Document

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+	
+	
+	public boolean isPrime(long n){
+	    if(n < 2) return false;
+	    if(n == 2 || n == 3) return true;
+	    if(n%2 == 0 || n%3 == 0) return false;
+	    long sqrtN = (long)Math.sqrt(n)+1;
+	    for(long i = 6; i <= sqrtN; i += 6) {
+	        if(n%(i-1) == 0 || n%(i+1) == 0) return false;
+	    }
+	    return true;	
+	}
+}
